f39e73325540ecec2a941a9144da997990a9107f
Lukas Zilka <lukas@zilka.me>
2015-03-05 10:01:44 +0100

    Grid processing.

--- exp/check.sh
+++ exp/check.sh
@@ -4,13 +4,17 @@ EXPERIMENT_NAME=$(basename "$1")
 
 EXPERIMENT_OUT=exp/results/${EXPERIMENT_NAME}
 
+extra=--print_header
+
 cfg_cnt=0
 while read cfg; do
-    echo $cfg
     cfg_cnt=$((cfg_cnt+1))
-    for i in 1 2 3 4 5 6 7 8 9 10; do
+    for i in 1 2 3 4 5; do
+        #echo -n "${i};${cfg};"
         eid=${EXPERIMENT_NAME}_cfg${cfg_cnt}_${i}
         STDERR_LOCATION=${EXPERIMENT_OUT}/${eid}.stderr.txt
-        bash exp/check_file.sh ${STDERR_LOCATION}
+        python xtrack_parse_log.py ${STDERR_LOCATION} $extra
+        extra=
+        #bash exp/check_file.sh ${STDERR_LOCATION}
     done
 done < $1
