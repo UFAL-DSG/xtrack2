1be98e0b55236d944a42194979bfd20f3d7a199d
Lukas Zilka <lukas@zilka.me>
2015-03-11 11:10:44 +0100

    Fixing dstc2 scorer.

--- score_dstc2.sh
+++ score_dstc2.sh
@@ -7,20 +7,18 @@ cd $DIR
 
 . ./config.sh
 
-if [ "$1" == "" ]; then
-    TRACK_FILE=$(mktemp xtrack_track.XXX.json)
-    cp /xdisk/data/dstc/xtrack/e2/valid.flist dstc_scripts/config/xtrack_e2_valid.flist
-    python xtrack2_dstc_tracker.py --data_file /xdisk/data/dstc/xtrack/e2/valid.json --output_file ${TRACK_FILE} --model_file $2
-
-    DATASET=xtrack_e2_valid
-else
-    echo "Using cmdline arguments."
-    TRACK_FILE=$1
-    DATASET=$2
-fi
+echo "Using cmdline arguments."
+TRACK_FILE=$1
+DATASET=$2
 
 SCORE_FILE=$(mktemp xtrack_score.XXX.csv)
 
-python dstc_scripts/score.py --dataset ${DATASET} --dataroot /xdisk/data/dstc/dstc2/data --trackfile ${TRACK_FILE} --ontology dstc_scripts/config/ontology_dstc2.json --scorefile ${SCORE_FILE}
+python dstc_scripts/score.py \
+    --dataset ${DATASET} \
+    --dataroot ${DATA_DIRECTORY}/dstc2/data \
+    --trackfile ${TRACK_FILE} \
+    --ontology dstc_scripts/config/ontology_dstc2.json \
+    --scorefile ${SCORE_FILE}
+
 python dstc_scripts/report.py --scorefile ${SCORE_FILE}
 echo $SCORE_FILE
